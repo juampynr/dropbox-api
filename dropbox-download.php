6e3b504fa7628d15930fb4eaf9a7d9f100f8a075
Juampy <juampynr@gmail.com>
2019-04-16 22:35:59 +0200

    Adjust autoload directory plus update DotEnv statements

--- dropbox-download.php
+++ dropbox-download.php
@@ -6,14 +6,14 @@
  * Usage: php dropbox-download.php [path to file at Dropbox] [path to destination including filename]
  */
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once './vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use Kunnu\Dropbox\Dropbox;
 use Kunnu\Dropbox\DropboxApp;
 
 // Load environment variables from a .env file if the file is present.
-$dotenv = Dotenv::create(__DIR__);
+$dotenv = new Dotenv('./');
 $dotenv->load();
 
 // Configure the Dropbox API handler.
@@ -21,4 +21,5 @@ $app = new DropboxApp(getenv('DROPBOX_CLIENT_ID'), getenv('DROPBOX_CLIENT_SECRET
 $dropbox = new Dropbox($app);
 
 // Download the file.
+$dropbox->listFolder('/');
 $file = $dropbox->download('/' . $argv[1], $argv[2]);
